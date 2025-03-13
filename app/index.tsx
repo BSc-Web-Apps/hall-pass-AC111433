aab372f88d5eafb2209f135902bf8786dcf1db45
AC111433 <ac111433@hc.ac.je>
2025-03-13 16:29:32 +0000

    feed the cat

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,21 @@
-import * as React from "react";
-import { View } from "react-native";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import { Text } from "~/components/ui/text";
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+import { Checkbox } from "~/components/ui/checkbox";
 
 export default function HomeScreen() {
+  const [checked, setChecked] = React.useState(false);
+
   return (
-    <View className="flex-1 justify-center items-center gap-5 p-6 bg-background">
-      <Card className="w-full max-w-sm p-6 rounded-2xl">
-        <CardHeader className="items-center">
-          <CardTitle className="pb-2 text-center">Hall Pass</CardTitle>
-          <Text>{new Date().toLocaleTimeString()}</Text>
-        </CardHeader>
+    <SafeAreaView className="flex flex-1 bg-black">
+      <View className="flex-row items-center w-full h-16 border-2 border-cyan-400">
+        <View className="w-12 h-full border-2 border-pink-400">
+          <Checkbox checked={checked} onCheckedChange={setChecked} />
+        </View>
 
-        <CardContent>
-          <View className="flex-row justify-center gap-3">
-            <View className="items-center">
-              <Text className="text-sm text-muted-foreground">An app for</Text>
-              <Text className="text-xl font-semibold">Students</Text>
-            </View>
-          </View>
-        </CardContent>
-
-        <CardFooter className="flex-col gap-3 pb-6">
-          <View className="flex-row items-center overflow-hidden">
-            <Text>Update me with your app code</Text>
-          </View>
-        </CardFooter>
-      </Card>
-    </View>
+        <View className="flex-1 h-full justify-center px-3 border-2 border-green-400">
+          <Text className="text-white">Feed the cat</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
