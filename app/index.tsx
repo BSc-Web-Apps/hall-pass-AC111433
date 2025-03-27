bcd2c075d53ec8f301c0e33d87c1f8c6087ab329
AC111433 <ac111433@hc.ac.je>
2025-03-27 15:29:16 +0000

    checkbox

--- app/index.tsx
+++ app/index.tsx
@@ -6,15 +6,10 @@ export default function HomeScreen() {
   const [checked, setChecked] = React.useState(false);
 
   return (
-    <SafeAreaView className="flex flex-1 bg-black">
-      <View className="flex-row items-center w-full h-16 border-2 border-cyan-400">
-        <View className="w-12 h-full border-2 border-pink-400">
-          <Checkbox checked={checked} onCheckedChange={setChecked} />
-        </View>
-
-        <View className="flex-1 h-full justify-center px-3 border-2 border-green-400">
-          <Text className="text-white">Feed the cat</Text>
-        </View>
+    <SafeAreaView className="flex-1 bg-black justify-center items-center">
+      <View className="w-4/5 border-2  rounded-lg p-4 flex-row items-center space-x-3 bg-gray-900">
+        <Checkbox checked={checked} onCheckedChange={setChecked} />
+        <Text className="text-white text-lg">Feed the cat</Text>
       </View>
     </SafeAreaView>
   );
